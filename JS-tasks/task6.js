2bb22a02a5fc105efcf4b2235141e6ea342caf8b
refren86 <denis1975shvaiber@gmail.com>
2022-04-01 23:44:12 +0300

    Added another solution to task 6

--- JS-tasks/task6.js
+++ JS-tasks/task6.js
@@ -8,11 +8,41 @@
 // var test1_3 = partialAny(test, 1, undefined, 3);
 // console.log(test1_3(5)); // a=1,b=5,c=3
 
-function partialAny(fn, ...args) {
+// Solution 1:
+// function partialAny(fn, ...args) {
+//   return (...args2) => {
+//     return fn(
+//       ...args.map((arg) => (arg === undefined ? args2.shift() : arg)),
+//       ...args2
+//     );
+//   };
+// }
+
+// Solution 2:
+const partialAny = (fn, ...args) => {
   return (...args2) => {
-    return fn(
-      ...args.map((arg) => (arg === undefined ? args2.shift() : arg)),
-      ...args2
-    );
+    const result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < args.length) {
+      let outerArg = args[i++];
+
+      if (outerArg === undefined) {
+        let innerArg = args2[j++];
+
+        result.push(innerArg);
+      } else {
+        result.push(outerArg);
+      }
+    }
+
+    while (j < args2.length) {
+      let innerArg = args2[j++];
+
+      result.push(innerArg);
+    }
+
+    return fn.apply(this, result);
   };
-}
+};
