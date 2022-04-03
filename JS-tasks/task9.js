1df3874c2d77815a3a92f2821db0cce768567cd7
refren86 <denis1975shvaiber@gmail.com>
2022-04-03 14:12:08 +0300

    Changed solution for task #9

--- JS-tasks/task9.js
+++ JS-tasks/task9.js
@@ -1,6 +1,18 @@
 // Напиши функцию filter(), которая принимает функцию-предикат и массив.
 // Возвращает она массив значений, для которых предикат вернет true.
 
+const isEven = (num) => num % 2 === 0;
+
 function filter(arr, fn) {
-  return arr.filter(fn);
+  let localArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      localArray.push(arr[i]);
+    }
+  }
+
+  return localArray;
 }
+
+console.log(filter([1, 2, 3, 4, 5], isEven));
